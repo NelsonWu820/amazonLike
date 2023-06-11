91a2838b248eb0c3730c91efd31c195e9e3a56f2
Nelson Wu <Nelsonwu820@gmail.com>
2023-06-11 10:42:50 -0400

    adding a desc for each route

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const router = express.Router();
 
+// @route GET api/auth
+// @desc 
+// @access Public
 router.get("/", async (req, res) => {
     console.log("started");
     res.send("started1");
