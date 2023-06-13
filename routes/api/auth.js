580f3a7f63b527dc7cf7fbb315a716b66164ef64
Nelson Wu <Nelsonwu820@gmail.com>
2023-06-13 11:59:04 -0400

    added a basic get route for the initial "/" to get user id

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,12 +1,19 @@
 const express = require("express");
 const router = express.Router();
+const check = require("express-validator");
+const auth = require("../../middleware/auth");
 
 // @route GET api/auth
 // @desc checks if the current jwt is valid and login them if it is
 // @access Public
-router.get("/", async (req, res) => {
-    console.log("started");
-    res.send("started1");
+router.get("/", auth, async (req, res) => {
+    try {
+        //finds user id using token
+        const user = await User.findbyId(req.user.id).select("-password");
+    } catch (error) {
+        console.error(error.message);
+        return res.status(400).json({ msg : "Server Error"});
+    }
 });
 
 // @route POST api/auth
