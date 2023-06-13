665dff74760d4e29242c620aca3ba62235f8328a
Nelson Wu <Nelsonwu820@gmail.com>
2023-06-13 10:01:57 -0400

    made a middleware to check jwt token

---
+++ middleware/auth.js
@@ -0,0 +1,26 @@
+const jwt = require("jsonwebtoken");
+const config = require("config");
+
+module.exports = function (req, res, next) {
+    const token = req.header("x-auth-token");
+
+    if (!token) {
+        return res.status(401).json({ msg: "No token, authorize denied"});
+    }
+
+    try{
+        jwt.verify(token, config.get("jwtSecret"), (error, decoded) => {
+            if(error){
+                return res.status(401).json({ msg: "invalid token"});
+            }
+            else{
+                req.user = decoded.user;
+                next();
+            }
+        })
+    }
+    catch(err){
+        console.error("Something went wrong in the server");
+        return res.status(500).json({ msg: "Server Error"})
+    }
+}
