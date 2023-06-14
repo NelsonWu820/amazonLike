9b179735edc68cb2110c936888dc11ce766022a9
Nelson Wu <Nelsonwu820@gmail.com>
2023-06-14 12:16:21 -0400

    adding a route to sign up

--- routes/api/user.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-// @route POST api/user
-// @desc signs up the user if the email is not used and is a valid with its gravatar and inputs it into mongodb
-// @access Public
-
-
-module.exports = router;
---
+++ routes/api/users.js
@@ -0,0 +1,83 @@
+const express = require("express");
+const router = express.Router();
+const { check, validationResult } = require("express-validator");
+const User = require("../../models/User");
+const gravatar = require("gravatar");
+const bcrypt = require("bcryptjs");
+const config = require("config");
+const jwt = require("jsonwebtoken");
+
+// @route POST api/user
+// @desc signs up the user if the email is not used and is a valid with its gravatar and inputs it into mongodb
+// @access Public
+router.post("/",
+    check("name", "Please input a valid name").notEmpty(),
+    check("email", "Please input a valid email address").isEmail(),
+    check("password", "Please input a password at least 6 characters long").isLength({ min: 6 }),
+    check("confirm", "Passwords don't match").equals("password"),
+    async (req, res) => {
+        //check for errors above
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
+        }
+
+        try {
+            //sends user data to mongodb through Schema
+            //takes it out from body
+            const { email, password, name } = req.body;
+    
+            // check if email is unique 
+            const user = await User.findOne({ email });
+            if (user) {
+                return res.status(400).json({ errors: "Email already has an account"});
+            }
+
+            //sets gravatar 
+            const avatar = gravatar.url(email, {
+                s: "200",
+                r: "pg",
+                d: "mm"
+            })
+    
+            //fill in the user Schema/ primes it
+            user = new User({
+                name,
+                email,
+                password,
+                avatar
+            })
+            
+            //sets password saved to encrypted password
+            const salt = await bcrypt.genSalt(10);
+            user.password = await bcrypt.hash(password, salt);
+
+            //then saves it actually
+            await user.save();
+
+            //creates jwt and stores it
+            //get user id
+            const payload = {
+                user: {
+                    id: user.id
+                }
+            }
+
+            //creates jwt
+            jwt.sign( payload, config.get("jwtSecret"), {expiresIn: "5 days" }, (err, token) => {
+                if (err) throw err;
+                res.json({token});
+            })
+
+        } catch (error) {
+            console.error(error.message);
+            return res.status(500).json({ errors: "Server Error"});
+        }
+        
+
+
+
+    }
+)
+
+module.exports = router;
