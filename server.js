cc40bdc0656291759fc28e9266b5090a68a2a667
Nelson Wu <Nelsonwu820@gmail.com>
2023-06-14 12:16:46 -0400

    added the line to allow middleware to run

--- server.js
+++ server.js
@@ -5,7 +5,10 @@ const app = express();
 
 connectDB();
 
+app.use(express.json({ extended: false}));
+
 app.use("/api/auth", require("./routes/api/auth"));
+app.use("/api/users", require("./routes/api/users"));
 
 const PORT = process.env.PORT || 5000;
 
