6d732deb5ce3c9f03b640ca797e444f8b832775e
Nelson Wu <Nelsonwu820@gmail.com>
2023-06-15 07:06:01 -0400

    getting rid of {extended: false} per Express documentation

--- server.js
+++ server.js
@@ -6,10 +6,13 @@ const app = express();
 
 connectDB();
 
-app.use(express.json({ extended: false}));
+app.use(express.json());
 
 app.use("/api/auth", require("./routes/api/auth"));
 app.use("/api/users", require("./routes/api/users"));
+app.use("/api/profile", require("./routes/api/profile"));
+app.use("/api/items", require("./routes/api/items"));
+app.use("/api/cart", require("./routes/api/cart"));
 
 const PORT = process.env.PORT || 5000;
 
