f73a80cb91cf5ab0c4bf88e5a049d149e43704c9
Nelson Wu <Nelsonwu820@gmail.com>
2023-06-15 09:37:13 -0400

    added a post for creating a profile or updating

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const auth = require("../../middleware/auth");
 const User = require("../../models/User");
 const Profile = require("../../models/Profile");
+const { check, validationResult } = require("express-validator");
 
 // @route GET api/profile
 // @desc gets the users profile info
@@ -10,7 +11,7 @@ const Profile = require("../../models/Profile");
 router.get("/me", auth, async (req, res) => {
     try {
         //find profile by the id which is created in the auth middleware
-        let profile = await Profile.findOne({user : req.user.id}).populate("user", ["name", "avatar"]);
+        const profile = await Profile.findOne({user : req.user.id}).populate("User", ["name", "avatar"]);
         
         //checks if profile has a user
         if (!profile){
@@ -25,7 +26,45 @@ router.get("/me", auth, async (req, res) => {
 })
 
 // @route POST api/profile
-// @desc edits a profiles info
+// @desc edits a profiles info or creates it
+// @access Private
+router.post("/edit", auth, 
+    check("address", "Please input a valid address").isLength({ min: 6 }).isString(),
+    async (req, res) => {
+        //checks validation
+        const check = validationResult(req);
+        if (!check){
+            return res.status(400).json({ errors: "Send valid address"});
+        }
+
+        //updates the profile
+        const { card1, card2, card3, sex, age } = req.body;
+        //create the profile
+        const profileField = {
+            user : req.user.id,
+            card1 : card1,
+            card2 : card2,
+            card3 : card3,
+            sex : sex,
+            age : age
+        }
+        //actually updates the profile and creates new profile if none are found
+        try {
+            const profile = await Profile.findOneAndUpdate(
+                {user: req.user.id},
+                {$set : profileField},
+                { new: true, upsert: true, setDefaultsOnInsert: true }
+            );
+            return res.json(profile);
+        } catch (error) {
+            console.error(error.message);
+            return res.status(500).json({ error : "Server Error"});
+        }
+    }
+)
+
+// @route DELETE api/profile
+// @desc Deletes profile and all posts also user
 // @access Private
 
 module.exports = router;
