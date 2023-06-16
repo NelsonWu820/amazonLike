2fa38366af04bc43f73e1de9f0d441bece6b7da6
Nelson Wu <Nelsonwu820@gmail.com>
2023-06-16 06:40:10 -0400

    created 2 routes, 1 for getting all items the other for creating an item

--- routes/api/items.js
+++ routes/api/items.js
@@ -1,9 +1,45 @@
 const express = require("express");
 const router = express.Router();
+const Items = require("../../models/Items.js");
 
 // @route GET api/items
 // @desc gives a list of items  
 // @access Public
+router.get("/", 
+    async (req, res) => {
+        //should get all items
+        const items = await Items.find({});
+        try {
+            return res.json(items);
+        } catch (error) {
+            console.error(error.message);
+            return res.status(500).json({ error : "Server Error"});
+        }
+    }
+)
+
+// @route POST api/items
+// @desc creates an item 
+// @access Public
+router.post("/", 
+    async (req, res) => {
+        const {rating, description, price} = req.body;
+        
+        try {
+            let items = new Items({
+                rating: rating,
+                description: description,
+                price: price
+            });
+            await items.save();
+            return res.json(items);
+            
+        } catch (error) {
+            console.error(error.message);
+            return res.status(500).json({ error : "Server Error"});
+        }
+    }
+)
 
 // @route POST api/items/comments/:id
 // @desc adds a comment with a star rating
