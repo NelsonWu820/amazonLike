dad1489e2c98afe7930146f4a1e95efca317b30e
Nelson Wu <Nelsonwu820@gmail.com>
2023-06-27 12:15:25 -0400

    added a cart schema

---
+++ models/Cart.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+
+const cartSchema = new mongoose.Schema({
+    user: {
+        type: mongoose.Schema.Types.ObjectId
+    },
+
+    items:[{
+        type: mongoose.Schema.Types.ObjectId
+    }]
+})
+
+module.exports = mongoose.model('cart', cartSchema);
