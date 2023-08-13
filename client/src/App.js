3d80b567d2b1d9231530c494eace9366a630ffae
Nelson Wu <Nelsonwu820@gmail.com>
2023-08-13 09:49:24 -0400

    added them using react router dom to lonk them in the navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,20 @@
 import './App.css';
+import {BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Landing from './components/layout/Landing';
+import Login from './components/auth/Login';
+import Register from './components/auth/Register';
+import Navbar from './components/layout/Navbar';
 
 function App() {
   return (
-    <div className="App">
-    </div>
+    <Router>
+      <Navbar/>
+      <Routes>
+        <Route path='/' element={ <Landing/>}/>
+        <Route path='/login' element= { <Login/>}/>
+        <Route path='/register' element = { <Register/> }/>
+      </Routes>
+    </Router>
   );
 }
 
