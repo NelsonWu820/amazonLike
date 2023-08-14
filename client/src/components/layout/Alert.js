cb4279b829ca501e15c9bd3924c161b39cb3e4d7
Nelson Wu <Nelsonwu820@gmail.com>
2023-08-14 07:17:42 -0400

    initalized Alert component which will show an array of alerts when used

---
+++ client/src/components/layout/Alert.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+
+const Alert = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+
+Alert.propTypes = {
+
+};
+
+
+export default Alert;
