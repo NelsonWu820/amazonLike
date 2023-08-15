f2469148bf852da6f89f69d9cf51c9e520eeebf9
Nelson Wu <Nelsonwu820@gmail.com>
2023-08-15 10:00:56 -0400

    renamed the function and exported it

--- client/src/reducers/alert.js
+++ client/src/reducers/alert.js
@@ -1,14 +1,16 @@
 import { ALERT_SET, ALERT_REMOVE } from "../actions/types";
-initialState = []
+const initialState = []
 
-export const alert = (state = initialState, action) => {
+function alertReducer(state = initialState, action) {
     const { type, payload } = action
     switch (type) {
         case ALERT_SET:
             return [...state, payload];
         case ALERT_REMOVE:
-            return state.filter((alert) => alert.id != payload.id);
+            return state.filter((alert) => alert.id !== payload.id);
         default:
             return state;
     }
 }
+
+export default alertReducer;
