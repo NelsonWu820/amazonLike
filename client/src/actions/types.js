eb316c41af5c4732fb98e4e2daa845ba2c0ff49e
Nelson Wu <Nelsonwu820@gmail.com>
2023-08-21 08:39:52 -0400

    adde register success fail and the same for load user

--- client/src/actions/types.js
+++ client/src/actions/types.js
@@ -2,3 +2,7 @@ export const ALERT_SET = 'ALERT_SET';
 export const ALERT_REMOVE = 'ALERT_REMOVE';
 export const LOGIN_SUCCESS = 'LOGIN_SUCCESS';
 export const LOGIN_FAIL = 'LOGIN_FAIL';
+export const REGISTER_SUCCESS = 'REGISTER_SUCCESS';
+export const REGISTER_FAIL = 'REGISTER_FAIL'; 
+export const USER_LOADED = 'USER_LOADED';
+export const AUTH_ERROR = 'AUTH_ERROR'; 
