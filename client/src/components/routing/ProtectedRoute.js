89109ecffeaf06badc3cc5246fc5eb7d93d566be
Nelson Wu <Nelsonwu820@gmail.com>
2023-08-30 08:35:06 -0400

    made a protected route where it checks if user isAuth if not it sends them back to login page

---
+++ client/src/components/routing/ProtectedRoute.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { Navigate } from 'react-router-dom';
+
+
+const ProtectedRoute = ({ component: Component, auth: { isAuthenticated }}) => {
+    if(isAuthenticated) return <Component/>
+    return (<Navigate to={"/login"} />)
+};
+
+
+ProtectedRoute.propTypes = {
+    auth: PropTypes.object.isRequired,
+};
+
+const mapStateToProps = state => ({
+    auth: state.auth
+})
+
+
+export default connect(mapStateToProps)(ProtectedRoute);
