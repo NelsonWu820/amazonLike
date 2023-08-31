0ed37421a55d27b8ac46354613210c20ada79e00
Nelson Wu <Nelsonwu820@gmail.com>
2023-08-31 10:11:04 -0400

    added auto check for loadUser and the dsahboard and profile forms

--- client/src/App.js
+++ client/src/App.js
@@ -7,11 +7,33 @@ import Navbar from './components/layout/Navbar';
 import Alert from './components/layout/Alert';
 import ProtectedRoute from './components/routing/ProtectedRoute';
 import Dashboard from './components/dashboard/Dashboard';
+import ProfileForm from './components/profile-form/ProfileForm';
+
+import setAuthToken from './utils/setAuthToken';
+import { LOGOUT } from './actions/types';
+import { loadUser } from './actions/auth';
 
 import store from './store';
 import { Provider } from 'react-redux';
+import { useEffect } from 'react';
 
 function App() {
+  useEffect(() => {
+    // check for token in LS when app first runs
+    if (localStorage.token) {
+      // if there is a token set axios headers for all requests
+      setAuthToken(localStorage.token);
+    }
+    // try to fetch a user, if no token or invalid token we
+    // will get a 401 response from our API
+    store.dispatch(loadUser());
+
+    // log user out from all tabs if they log out in one tab
+    window.addEventListener('storage', () => {
+      if (!localStorage.token) store.dispatch({ type: LOGOUT });
+    });
+  }, []);
+
   return (
     <Provider store={store}>
       <Router>
@@ -19,9 +41,11 @@ function App() {
         <Alert/>
         <Routes>
           <Route path='/' element={ <Landing/>}/>
-          <Route path='/login' element= { <Login/>}/>
-          <Route path='/register' element = { <Register/> }/>
-          <Route path='/dashboard' element={<ProtectedRoute component={<Dashboard/>}/>}/>
+          <Route path='login' element= { <Login/>}/>
+          <Route path='register' element = { <Register/> }/>
+          <Route path="dashboard" element={<ProtectedRoute component={Dashboard} />}/>
+          <Route path="create-profile" element={<ProtectedRoute component={ProfileForm} />}/>
+          <Route path="edit-profile" element={<ProtectedRoute component={ProfileForm} />}/>
         </Routes>
       </Router>
     </Provider>
