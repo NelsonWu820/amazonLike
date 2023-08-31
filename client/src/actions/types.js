92ce9bd41d32bdc39f62d99595bbafc92a7d499b
Nelson Wu <Nelsonwu820@gmail.com>
2023-08-31 10:08:14 -0400

    added account delete type

--- client/src/actions/types.js
+++ client/src/actions/types.js
@@ -10,3 +10,4 @@ export const LOGOUT = 'LOGOUT';
 export const PROFILE_GET = 'PROFILE_GET';
 export const PROFILE_ERROR = 'PROFILE_ERROR';
 export const PROFILE_UPDATE = 'PROFILE_UPDATE';
+export const ACCOUNT_DELETE = 'ACCOUNT_DELETE';
