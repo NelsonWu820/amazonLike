8b92833a1349751ce5419a9b592e497c5646101f
Nelson Wu <Nelsonwu820@gmail.com>
2023-09-07 10:08:46 -0400

    placeholder for all comments

---
+++ client/src/components/item/ItemComment.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+
+const ItemComment = () => {
+    return (
+        <div>
+            Comment
+        </div>
+    );
+};
+
+
+ItemComment.propTypes = {
+
+};
+
+
+export default ItemComment;
