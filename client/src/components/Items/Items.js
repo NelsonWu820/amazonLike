30bf606c43d06d1fe7c9a7e77d260cc60427176b
Nelson Wu <Nelsonwu820@gmail.com>
2023-09-07 10:08:05 -0400

    changes itemHolder inputs to whole item state

--- client/src/components/Items/Items.js
+++ client/src/components/Items/Items.js
@@ -10,11 +10,14 @@ const Items = ({ item: {items}, getAllItems}) => {
         getAllItems();
     },[getAllItems])
 
+    
+
+
     return (
         <section className="py-5">
             <div className="container px-4 px-lg-5 mt-5">
                 <div className="row gx-4 gx-lg-5 row-cols-2 row-cols-md-3 row-cols-xl-4 justify-content-center">
-                    {items.map((itemCover) => <ItemsHolder title={itemCover.title} image={itemCover.image} price={itemCover.price} id={itemCover.id}/>)}
+                    {items.map((itemCover) => <ItemsHolder item={itemCover} key={itemCover._id}/>)}
                 </div>
             </div>
         </section>
