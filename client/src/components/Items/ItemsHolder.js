c6b56c928c93e4c7c21b1592f4beec36f41862cd
Nelson Wu <Nelsonwu820@gmail.com>
2023-09-07 10:08:30 -0400

    changed input to item state

--- client/src/components/Items/ItemsHolder.js
+++ client/src/components/Items/ItemsHolder.js
@@ -3,8 +3,7 @@ import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 
 
-const ItemsHolder = ({title, image, price, id}) => {
-    return (
+const ItemsHolder = ({item : {title, image, price, _id}}) => (
         <div className="col mb-5">
             <div className="card h-100">
                 <img className="card-img-top itemHolder" src={image} alt="..." />
@@ -15,19 +14,16 @@ const ItemsHolder = ({title, image, price, id}) => {
                     </div>
                 </div>
                 <div className="card-footer p-4 pt-0 border-top-0 bg-transparent">
-                    <div className="text-center"><Link className="btn btn-outline-dark mt-auto" to={`/item/${id}`}>View Details</Link></div>
+                    <div className="text-center"><Link className="btn btn-outline-dark mt-auto" to={`/item/${_id}`}>View Details</Link></div>
                     <div className="text-center"><a className="btn btn-outline-dark mt-auto" >Add To Cart</a></div>
                 </div>
             </div>
         </div>
     );
-};
 
 
 ItemsHolder.propTypes = {
-    title: PropTypes.string,
-    image: PropTypes.string,
-    price: PropTypes.number,
+    item: PropTypes.object.isRequired,
 };
 
 
