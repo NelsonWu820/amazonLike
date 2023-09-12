171b82a65a554d755fc791a0475808493ac8170c
Nelson Wu <Nelsonwu820@gmail.com>
2023-09-12 06:42:41 -0400

    added the delete comment action made a check state that will decide wehter the user can post a comment or see old comment to delete

--- client/src/components/item/ItemCommentForm.js
+++ client/src/components/item/ItemCommentForm.js
@@ -1,41 +1,41 @@
 import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-import {addComment} from '../../actions/item'
+import {addComment, deleteComment} from '../../actions/item'
 
 
-const ItemCommentForm = ({addComment, id, auth: {user: {_id}}, item: {item}}) => {
+const ItemCommentForm = ({addComment, deleteComment, id, auth: {user: {_id}}, item: {item}}) => {
     const [formData, setFormData] = useState({
         text: '',
         rating: 0
     });
-    
-    let commentText = '';
-    let commentRating = 0;
-    let answer = false
 
+    //used to rerender state when adding or deleteing comment
+    const[check, setCheck] = useState(false)
+
+    let answer = false;
+    
+    //if the have a commment already fill in the state and don't let them edit it only delete
     const checkComment = (user_id) => {
         item.comments.map((comment) => {
             if(user_id.toString() === comment.user.toString()){
-                commentText = comment.text;
-                commentRating = comment.rating;
+                setFormData({...formData, text: comment.text, rating: comment.rating})
+                setCheck(true)
                 answer = true;
-                return;
             }
         })
-        return answer
+        setCheck(answer);
+        return answer;
     }
 
     useEffect(() => {
-        if(checkComment(id) === true){
-            setFormData({...formData, text: commentText, rating: commentRating})
-        }
+        checkComment(_id)
     },[])
 
     const {text, rating} = formData;
 
     const onClick = (num) => {
-        setFormData({rating: num});
+        setFormData({...formData, rating: num});
 
     }
 
@@ -55,38 +55,51 @@ const ItemCommentForm = ({addComment, id, auth: {user: {_id}}, item: {item}}) =>
         setFormData({...formData, [e.target.name]: e.target.value})
     }
 
+
+    let comment_id = '';
+    const deleteCommentById = (user_id) => {
+        item.comments.map((comment) => {
+            if(user_id.toString() === comment.user.toString()){
+                console.log('id: ' + id + 'comment_id: ' + comment._id)
+                comment_id = comment._id;
+            }
+        })
+        deleteComment(id, comment_id)
+    }
+
     return (
         <div>
-            {checkComment(_id) === true? (
+            { check === true? (
                 <div>
-                    <h1>Works</h1>
-                    <form onSubmit={(e) => {
-                        e.preventDefault();
-                        addComment(id, formData);
-                    }}>
-                        <input placeholder='Add a comment' type='text' name='text' value={text} onChange={onChange}/>
-                        <div className="ratings-wrapper">
-                        <div data-productid={id} className="ratings">
-                                <span style={changeStar(5)} onClick={() => onClick(5)}>★</span>
-                                <span style={changeStar(4)} onClick={() => onClick(4)}>★</span>
-                                <span style={changeStar(3)} onClick={() => onClick(3)}>★</span>
-                                <span style={changeStar(2)} onClick={() => onClick(2)}>★</span>
-                                <span style={changeStar(1)} onClick={() => onClick(1)}>★</span>
+                    <div className="ratings-wrapper">
+                        <div className="ratings">
+                                <span style={changeStar(5)}>★</span>
+                                <span style={changeStar(4)}>★</span>
+                                <span style={changeStar(3)}>★</span>
+                                <span style={changeStar(2)}>★</span>
+                                <span style={changeStar(1)}>★</span>
                             </div>
                         </div>
-                        <input type="submit" value='Submit'/>
-                    </form>
+                    <input disabled={true} placeholder='Add a comment' type='text' name='text' value={text}/>
+                    <div>
+                        <button type='delete' onClick={() => {
+                            deleteCommentById(_id);
+                            setFormData({ text: '', rating: 0});
+                            setCheck(false);
+                        }}>
+                            Delete
+                        </button>
+                    </div>
                 </div>
             ):(
                 <div>
-                    <h1>dosen't</h1>
                     <form onSubmit={(e) => {
                         e.preventDefault();
                         addComment(id, formData);
-                    }}>
-                        <input placeholder='Add a comment' type='text' name='text' value={text} onChange={onChange}/>
+                        setCheck(true)
+                        }}>
                         <div className="ratings-wrapper">
-                        <div data-productid={id} className="ratings">
+                            <div className="ratings">
                                 <span style={changeStar(5)} onClick={() => onClick(5)}>★</span>
                                 <span style={changeStar(4)} onClick={() => onClick(4)}>★</span>
                                 <span style={changeStar(3)} onClick={() => onClick(3)}>★</span>
@@ -94,6 +107,7 @@ const ItemCommentForm = ({addComment, id, auth: {user: {_id}}, item: {item}}) =>
                                 <span style={changeStar(1)} onClick={() => onClick(1)}>★</span>
                             </div>
                         </div>
+                        <input placeholder='Add a comment' type='text' name='text' value={text} onChange={onChange}/>
                         <input type="submit" value='Submit'/>
                     </form>
                 </div>
@@ -105,6 +119,7 @@ const ItemCommentForm = ({addComment, id, auth: {user: {_id}}, item: {item}}) =>
 
 ItemCommentForm.propTypes = {
     addComment: PropTypes.func.isRequired,
+    deleteComment: PropTypes.func.isRequired,
 };
 
 const mapStateToProps = state => ({
@@ -112,4 +127,4 @@ const mapStateToProps = state => ({
     item: state.item
 })
 
-export default connect(mapStateToProps, {addComment})(ItemCommentForm);
+export default connect(mapStateToProps, {addComment, deleteComment})(ItemCommentForm);
