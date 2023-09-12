cea43130aef0ddedc77e4a4d857c6c76c4d57dc5
Nelson Wu <Nelsonwu820@gmail.com>
2023-09-12 06:44:11 -0400

    placeholder to check if comments display the right data and amounts

--- client/src/components/item/ItemComment.js
+++ client/src/components/item/ItemComment.js
@@ -1,18 +1,26 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import Rating from '../rating/Rating';
+import formatDate from '../../utils/formatDate';
 
 
-const ItemComment = () => {
+const ItemComment = ({comment: {name, avatar, rating, text, date}}) => {
+
     return (
+
         <div>
-            Comment
+            <h1>{name}</h1>
+            <img src={avatar}/>
+            <Rating rating={rating}/>
+            <div>{text}</div>
+            <div>{formatDate(date)}</div>
         </div>
     );
 };
 
 
 ItemComment.propTypes = {
-
+    comment: PropTypes.object.isRequired,
 };
 
 
