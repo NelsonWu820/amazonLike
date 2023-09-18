4d138ed50940f9a5ac5d54bd6f04b502d8c14451
Nelson Wu <Nelsonwu820@gmail.com>
2023-09-18 19:06:07 -0400

    added cart get action and correct cart item added and delete then history get and add actions

--- client/src/reducers/cart.js
+++ client/src/reducers/cart.js
@@ -1,23 +1,63 @@
-import { CART_ITEM_ADDED, CART_ITEM_DELTED, HISTORY_ITEM_ADDED, HISTORY_ITEM_DELTED } from "../actions/types";
+import { CART_GET, CART_ITEM_ADDED, CART_ITEM_DELTED, HISTORY_GET, HISTORY_ITEM_ADDED } from "../actions/types";
 
 const initialState = {
     cart: [],
     history: [],
-    loading: true
+    loading: true,
+    historyLoading: true,
 }
 
 function cartReducer(state = initialState, action){
     const { type, payload } = action
     switch (type){
-        case CART_ITEM_ADDED:
+        case CART_GET:
             return{
                 ...state,
-                cart: [...state.cart, ...payload],
+                cart: payload,
+                loading: false
+            }
+        case CART_ITEM_ADDED:
+            const itemsState = payload.map((item) => ({
+                image: item.image,
+                title: item.title,
+                price: item.price,
+                rating: item.rating,
+              }));
+
+            return{
+                ...state,
+                cart: [...itemsState, ...state.cart],
                 loading: false
             }
         case CART_ITEM_DELTED:
-            return state.cart.filter((id) => id !== payload)
-            
+            const newState = state.cart.filter((item) =>(
+                item._id !== payload
+            ))
+            return{
+                ...state,
+                cart: [...newState]
+                }
+        case HISTORY_GET:
+            return{
+                ...state,
+                history: payload,
+                historyLoading: false
+            }
+        case HISTORY_ITEM_ADDED:
+            const cartState = payload.map((item) => ({
+                id: item.id,
+                image: item.image,
+                title: item.title,
+                price: item.price,
+                rating: item.rating,
+                tag: item.tag
+              }));
+            return{
+                ...state,
+                cart: [],
+                history: [...cartState, ...state.history],
+                historyLoading: false
+            }
         default:
             return state
     }
