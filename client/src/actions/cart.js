8ec944017a8450eab55a8879023077564ae24dec
Nelson Wu <Nelsonwu820@gmail.com>
2023-09-18 19:00:35 -0400

    added the get cart action and get history and add history actions

--- client/src/actions/cart.js
+++ client/src/actions/cart.js
@@ -1,7 +1,23 @@
-import { CART_ITEM_ADDED, CART_ITEM_DELTED, HISTORY_ITEM_ADDED, HISTORY_ITEM_DELTED } from "../actions/types";
+import { CART_GET, CART_ITEM_ADDED, CART_ITEM_DELTED, HISTORY_GET, HISTORY_ITEM_ADDED } from "../actions/types";
 import { setAlert } from "./alert";
 import api from '../utils/api';
 
+//get all items in cart db
+export const getCart = () => async dispatch => {
+
+    try {
+        const res = await api.get('/cart');
+
+        dispatch({
+            type: CART_GET,
+            payload: res.data
+        });
+    } catch (err) {
+        console.log("Add Cart Error:", err);
+    }
+} 
+
+//add item in cart by amount and the id
 export const cartAddItem = (amount, itemId) => async dispatch => {
 
         try {
@@ -18,11 +34,12 @@ export const cartAddItem = (amount, itemId) => async dispatch => {
     }
 } 
 
+
 //deletes an item in cart
-export const cartRemoveItem = (itemId) => async dispatch => {
+export const cartRemoveItem = (id) => async dispatch => {
 
         try {
-            const res = await api.delete(`/cart/${itemId}`);
+            const res = await api.delete(`/cart/${id}`);
 
             dispatch({
                 type: CART_ITEM_DELTED,
@@ -32,5 +49,36 @@ export const cartRemoveItem = (itemId) => async dispatch => {
             console.log("Delete Cart Error:", err);
     
         
+    }
+} 
+
+//takes from cart and places it into history
+export const historyAddItem = (cart) => async dispatch => {
+
+    try {
+
+        let jsonCart = JSON.stringify(cart);
+        const res = await api.post('/cart/history', jsonCart);
+
+        dispatch({
+            type: HISTORY_ITEM_ADDED,
+            payload: res.data
+        });
+    } catch (err) {
+        console.log("Add History Error:", err);
+    }
+} 
+
+//get history
+export const getHistory = () => async dispatch => {
+    try {
+        const res = await api.get('/cart/history');
+
+        dispatch({
+            type: HISTORY_GET,
+            payload: res.data
+        });
+    } catch (err) {
+        console.log("Add History Error:", err);
     }
 } 
