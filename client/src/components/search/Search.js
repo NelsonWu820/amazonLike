8639430349c6dfcd17b730ce6a7f903bd43f11e5
Nelson Wu <Nelsonwu820@gmail.com>
2023-09-18 20:34:45 -0400

    place holder for search component

---
+++ client/src/components/search/Search.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Search = () => {
+    return (
+        <div>
+            <h1>Search</h1>
+        </div>
+    );
+};
+
+
+Search.propTypes = {
+
+};
+
+
+export default Search;
