daa7e60b2c4c7101f0df5c542b8d31706a790f09
Nelson Wu <Nelsonwu820@gmail.com>
2023-09-20 06:37:06 -0400

    made the search component that maps through stae and calls Items Holder component

--- client/src/components/search/Search.js
+++ client/src/components/search/Search.js
@@ -1,18 +1,35 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import PropTypes from 'prop-types';
+import { useParams } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { getItemByTag } from '../../actions/item';
+import ItemsHolder from '../Items/ItemsHolder'
 
-const Search = () => {
+const Search = ({item: {items}, getItemByTag}) => {
+    const { tag } = useParams();
+    useEffect(() => {
+        getItemByTag(tag);
+    },[getItemByTag, tag]);
+
     return (
-        <div>
-            <h1>Search</h1>
-        </div>
+        <section className="py-5">
+            <div className="container px-4 px-lg-5 mt-5">
+                <div className="row gx-4 gx-lg-5 row-cols-2 row-cols-md-3 row-cols-xl-4 justify-content-center">
+                    {items.map((itemCover) => <ItemsHolder item={itemCover} key={itemCover._id}/>)}
+                </div>
+            </div>
+        </section>
     );
 };
 
 
 Search.propTypes = {
-
+    item: PropTypes.object.isRequired,
+    getItemByTag: PropTypes.func.isRequired,
 };
 
+const mapStateToProps = state =>({
+    item: state.item
+})
 
-export default Search;
+export default connect(mapStateToProps, {getItemByTag})(Search);
