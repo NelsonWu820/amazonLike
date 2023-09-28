0db5e8074935f492e048394807500ab20ba14db5
Nelson Wu <Nelsonwu820@gmail.com>
2023-09-28 07:09:25 -0400

    the same as Rating.js but with front page specific styling

---
+++ client/src/components/rating/RatingItems.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+
+const RatingItems = ({rating}) => {
+    return (
+        <div className="ratings-comment-section-holder">
+            <div className="ratings-comment-section">
+                    <span style={{color: rating >= 1 ? "orange": "black"}}>★</span>
+                    <span style={{color: rating >= 2 ? "orange": "black"}}>★</span>
+                    <span style={{color: rating >= 3 ? "orange": "black"}}>★</span>
+                    <span style={{color: rating >= 4 ? "orange": "black"}}>★</span>
+                    <span style={{color: rating >= 5 ? "orange": "black"}}>★</span>
+            </div>
+        </div>
+    );
+};
+
+
+RatingItems.propTypes = {
+    rating: PropTypes.number,
+};
+
+
+export default RatingItems;
