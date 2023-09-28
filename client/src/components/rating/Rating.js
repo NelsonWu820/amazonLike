02fff9d065768209ee53111f52dd0171f95bf267
Nelson Wu <Nelsonwu820@gmail.com>
2023-09-28 07:02:44 -0400

    changed styling

--- client/src/components/rating/Rating.js
+++ client/src/components/rating/Rating.js
@@ -1,15 +1,17 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 
 const Rating = ({rating}) => {
 
     return (
-        <div className="ratings">
-                <span style={{color: rating >= 5 ? "orange": "black"}}>★</span>
-                <span style={{color: rating >= 4 ? "orange": "black"}}>★</span>
-                <span style={{color: rating >= 3 ? "orange": "black"}}>★</span>
-                <span style={{color: rating >= 2 ? "orange": "black"}}>★</span>
-                <span style={{color: rating >= 1 ? "orange": "black"}}>★</span>
+        <div>
+            <div className="ratings-comment-section">
+                    <span style={{color: rating >= 1 ? "orange": "black"}}>★</span>
+                    <span style={{color: rating >= 2 ? "orange": "black"}}>★</span>
+                    <span style={{color: rating >= 3 ? "orange": "black"}}>★</span>
+                    <span style={{color: rating >= 4 ? "orange": "black"}}>★</span>
+                    <span style={{color: rating >= 5 ? "orange": "black"}}>★</span>
+            </div>
         </div>
     );
 };
