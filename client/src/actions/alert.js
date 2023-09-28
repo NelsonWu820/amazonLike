05b302e94102d4338d55e6705132e469a3620b64
Nelson Wu <Nelsonwu820@gmail.com>
2023-09-28 07:09:54 -0400

    changed how long alerts show from 5 sec to 2.5 secs

--- client/src/actions/alert.js
+++ client/src/actions/alert.js
@@ -1,7 +1,7 @@
 import { ALERT_SET, ALERT_REMOVE } from "./types";
 import {v4 as uuidv4} from 'uuid';
 
-export const setAlert = (msg, alertType, timeout = 5000) => dispatch => {
+export const setAlert = (msg, alertType, timeout = 2500) => dispatch => {
     const id = uuidv4(); 
 
     dispatch({
