3fa0932e5b88a32398cc21efaa818b933d644529
Nelson Wu <Nelsonwu820@gmail.com>
2023-09-28 07:10:03 -0400

    added stlying

--- client/src/components/item/Item.js
+++ client/src/components/item/Item.js
@@ -23,11 +23,22 @@ const Item = ({getItemById, auth: {isAuthenticated}, item:{item, loading}}) => {
                 ): (
                     <div>Please login to make an account but have it with comment form inside the comment and login link</div>
                 )}
-                {item.comments.length > 0 ? (
-                    item.comments.map((comment) => <ItemComment comment={comment}/>)
-                ): (
-                    <h1>No comments yet</h1>
-                )}
+                    <div className="container justify-content-center mb-100">
+                        <div className="row">
+                            <div className="col-md-12">
+                                <div className="card-comments">
+                                    <div className="card-body-comments">
+                                        <h4 className="card-title">Recent Comments</h4>
+                                        <h6 className="card-subtitle">Latest Comments section by users</h6> </div>
+                                        {item.comments.length > 0 ? (
+                                            item.comments.map((comment) => <ItemComment comment={comment}/>)
+                                        ): (
+                                            <h1>No comments yet</h1>
+                                        )}
+                                </div>
+                            </div>
+                        </div>                     
+                    </div>
                 <footer className="py-5 bg-dark">
                     <div className="container"><p className="m-0 text-center text-white">Copyright &copy; Your Website 2023</p></div>
                 </footer>
