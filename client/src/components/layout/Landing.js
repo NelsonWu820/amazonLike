6a16acf0a45ee607a8e094ec721486e7eb76f4c5
Nelson Wu <Nelsonwu820@gmail.com>
2023-09-30 08:02:29 -0400

    added a footer

--- client/src/components/layout/Landing.js
+++ client/src/components/layout/Landing.js
@@ -16,9 +16,9 @@ const Landing = () => {
 
             <Items/>
 
-            <footer className="py-5 bg-dark">
-                <div className="container"><p className="m-0 text-center text-white">Copyright &copy; AmazonLike 2023</p></div>
-            </footer>
+            <div className="py-5 bg-dark footer-container">
+                <div ><p className="m-0 text-center text-white">Copyright &copy; AmazonLike 2023 - Published by Nelson Wu</p></div>
+            </div>
             <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
             <script src="js/scripts.js"></script>
         </div>
