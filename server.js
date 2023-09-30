560d2e2a8d92db6f9996b47673559a6fe56eab36
Nelson Wu <Nelsonwu820@gmail.com>
2023-09-30 11:09:32 -0400

    added productiion defalut file

--- server.js
+++ server.js
@@ -1,19 +1,34 @@
 const express = require("express");
 const connectDB = require("./config/db");
-
+const path = require('path');
 
 const app = express();
 
+//connect to db
 connectDB();
 
+//lets middleware run byt initalizing it
 app.use(express.json());
 
+//routes defined
 app.use("/api/auth", require("./routes/api/auth"));
 app.use("/api/users", require("./routes/api/users"));
 app.use("/api/profile", require("./routes/api/profile"));
 app.use("/api/cart", require("./routes/api/cart"));
 app.use("/api/items", require("./routes/api/items"));
 
+// Serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+    // Set static folder
+    app.use(express.static('client/build'));
+  
+    //gets the index.html from client/build
+    app.get('*', (req, res) => {
+      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+}
+
+//checks for open port if none set to 5000 by default
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server started on Port ${PORT}`));
