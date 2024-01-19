5625c8973c8cc91eb90c2d43031cb90293bbdf20
Nelson Wu <Nelsonwu820@gmail.com>
2024-01-19 10:03:13 -0500

    added cors header

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const express = require("express");
 const connectDB = require("./config/db");
 const path = require('path');
+const cors = require('cors');
 
 const app = express();
 
